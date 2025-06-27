5f692abb2c3adb24e1949258eb9136275e06bddf
Harshal Margaj <harshal.margaj12@gmail.com>
2025-06-27 18:46:07 +0530

    split code

--- app/dashboard/_components/Card.jsx
+++ app/dashboard/_components/Card.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Activity } from "lucide-react";
 
-const Card = () => {
-	return <div>Card</div>;
+const Card = ({ metric }) => {
+	return (
+		<div className="border border-neutral-100 w-[25%] shadow-md rounded-md p-2 space-y-2 text-gray-700">
+			<div>{metric.icon}</div>
+
+			<div className="font-semibold">{metric.title}</div>
+			<div className="text-5xl">{metric.value}</div>
+
+			<div className="flex items-center justify-between">
+				<div className="text-sm">Since last month</div>
+				<div className="text-green-500 flex items-center gap-2">
+					25% <Activity />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
---
+++ constants/charts.js
@@ -0,0 +1,9 @@
+export const memberGrowthData = [
+	{ name: "Mon", members: 20 },
+	{ name: "Tue", members: 45 },
+	{ name: "Wed", members: 32 },
+	{ name: "Thu", members: 51 },
+	{ name: "Fri", members: 42 },
+	{ name: "Sat", members: 65 },
+	{ name: "Sun", members: 78 },
+];
